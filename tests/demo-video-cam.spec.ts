b07033a18b510a855227a7bd13d7f887fa11d2b4
Atthaboon Sanurt <atthaboon.s@qahive.com>
2022-06-11 15:37:11 +0700

    Clean code

--- tests/demo-video-cam.spec.ts
+++ tests/demo-video-cam.spec.ts
@@ -1,5 +1,7 @@
 import { test, expect, Page } from '@playwright/test';
 
+// Please create the demo.y4m file by running this command first
+// $ ffmpeg -i demo-video.mp4  demo.y4m
 test('able to use fake web cam', async ({ page }) => {
   await page.goto('https://davidwalsh.name/demo/camera.php');
   await page.pause();
